9b831761dc0713d303510ac367ebbf166eb1c6dd
lekevin <lekevvin@gmail.com>
2022-12-08 11:00:04 -0600

    added functionalities to studypage and ui edits

--- quiz-app/Views/StudyPage.xaml.cs
+++ quiz-app/Views/StudyPage.xaml.cs
@@ -1,9 +1,25 @@
 namespace quiz_app.Views;
-
 public partial class StudyPage : ContentPage
 {
 	public StudyPage()
 	{
 		InitializeComponent();
 	}
+
+    private void OnCardTapped(object sender, EventArgs e)
+    {
+        var label = sender as Label;
+
+        if (Color.FromHex("#554971").Equals(Color.FromHex(label.BackgroundColor.ToString())))
+        {
+            label.RotateYTo(360, 200);
+            label.BackgroundColor = Colors.LightGray;
+            label.Text = "sup bruh";
+        }
+        else
+        {
+            // Background color is not #554971
+        }
+
+    }
 }
